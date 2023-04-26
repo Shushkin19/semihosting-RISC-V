b5da8c0433049a978cbccc96e40fef9cece75b6e
Shushkin19 <atomatic19@gmail.com>
2023-04-26 17:24:08 +0300

    add write file

--- semihost/file/main.c
+++ semihost/file/main.c
@@ -1,16 +1,42 @@
 #include <stdio.h>
-
 #include "syscalls.h"
 
-//extern void initialise_monitor_handles(void);
+#define SEMIHOSTING_SYS_FILE_MODE_WRITE 6
 
 int main(){
-    //initialise_monitor_handles();
-    //printf("OOOOOOHhhh!\n");
-asm volatile(
-    "addi t0, zero, 4\n"
-    "slli x0, x0, 0x1f \n"
-);
-sh_print("Hello, semihosting!!!\n");
+    int file_handler_for_write;
+    
+    if( ( file_handler_for_write=sh_fopen("//home//ubuntu//code//risc-v//one//semihost//file//write.txt", SEMIHOSTING_SYS_FILE_MODE_WRITE) ) == -1 ) {
+    
+    sh_print("Cannot open file.\n");
+    
+    } 
+    
+    else {
+        
+        sh_fwrite(file_handler_for_write,"Hello RISC-V\n");
 
+    }
+    sh_fclose(file_handler_for_write);
+
+
+    int file_handler;
+    
+    if( ( file_handler=sh_fopen("//home//ubuntu//code//risc-v//one//semihost//file//read.txt", 2) ) == -1 ) {
+    
+    sh_print("Cannot open file.\n");
+    
+    } 
+    else {
+        int len = sh_flen(file_handler);
+        uint8_t str[24];
+        int read_byte = sh_fread(file_handler,str,len+1);
+       sh_print(str);
+    }
+
+    sh_fclose(file_handler);
+
+
+
+    return 0;
 }

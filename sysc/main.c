785e957c3140733a54be330f36c6b48cbcff8d38
Artemy Shushkin <atomatic19@gmail.com>
2023-06-22 11:48:36 +0300

    test

--- sysc/main.c
+++ sysc/main.c
@@ -1,54 +1,56 @@
 #include <stdio.h>
 #include <malloc.h>
 #include "syscalls.h"
-
+#include <errno.h>
 int main()
 {
 
     char data[100] = {0};
-    /*
-       FILE *file;
-      if ((file = fopen("//home//shusha//Code//RISC-V//Learn//semihosting-RISC-V//sysc//for_read.txt", "r")) == NULL)
-      {
-          printf("Error with file r\n");
-      }
-      else
-      {
 
-          fseek(file, 0, SEEK_END);
-          int size_file = ftell(file);
-          fseek(file, 0, SEEK_SET);
-          fgets(data, size_file, file);
-
-          fclose(file);
-          printf("%s\n", data);
-     */
-
-    FILE *wrt = NULL;
-    if ((wrt = fopen("//home//shusha//Code//RISC-V//Learn//semihosting-RISC-V//sysc//for_write.txt", "w")) == NULL)
+    FILE *file;
+    if ((file = fopen("//home//shusha//Code//RISC-V//Learn//semihosting-RISC-V//sysc//for_read.txt", "r")) == NULL)
     {
-        printf("Error with file w\n");
+        printf("Error with file r\n");
     }
     else
     {
 
-        int i = 0;
-        while (data[i] != "\n")
+        fseek(file, 0, SEEK_END);
+        int size_file = ftell(file);
+        fseek(file, 0, SEEK_SET);
+        fgets(data, size_file, file);
+        fclose(file);
+        printf("%s\n", data);
+
+        FILE *wrt;
+        if ((wrt = fopen("//home//shusha//Code//RISC-V//Learn//semihosting-RISC-V//sysc//for_write.txt", "w")) == NULL)
         {
-            if (data[i] == " ")
-            {
-                fprintf(wrt, "%s", "\n");
-            }
-            else
-            {
-                fprintf(wrt, "%c", data[i]);
-            }
-            i++;
+
+            printf("Error with file w: \n");
+            int x = errno;
+            // x = error_t;
+            x = __errno;
         }
-        fprintf(wrt, "%s", "\nThis is just an example :)");
+        else
+        {
+
+            int i = 0;
+            while (data[i] != '\0')
+            {
+                if (data[i] == ' ')
+                {
+                    fprintf(wrt, "%s", "\n");
+                }
+                else
+                {
+                    fprintf(wrt, "%c", data[i]);
+                }
+                i++;
+            }
+            fprintf(wrt, "%s", "\nThis is just an example :)");
+        }
+        fclose(wrt);
     }
-    fclose(wrt);
-    // }
 
     int a = 10;
     printf("%d\n ", a);
